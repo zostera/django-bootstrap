4942bf3efc83d985571e7da04b4f13f150ae5433
Dylan Verheul <dylan@zostera.nl>
2020-03-22 07:06:35 +0100

    Add basic structure

---
+++ tests/test_template_tags.py
@@ -0,0 +1,17 @@
+from django.template import Context, Template
+from django.test import TestCase
+
+TEMPLATE_BASE = ""
+
+
+def render_template(content, **context_args):
+    """Create a template that loads the template base and adds the given content."""
+    template = Template(f"{TEMPLATE_BASE}{content}")
+    return template.render(Context(context_args))
+
+
+class TemplateTagsTest(TestCase):
+    """Test template tags."""
+
+    def test_empty_template(self):
+        self.assertEqual(render_template(""), "")
