daa990ad9a9a4fa41487187246086aee41e8d36d
Dylan Verheul <dylan@zostera.nl>
2020-06-06 14:43:35 +0200

    Add hidden widget

---
+++ tests/test_widget_hidden_input.py
@@ -0,0 +1,34 @@
+from django import forms
+
+from tests.test_bootstrap_field import BootstrapFieldTestCase
+
+
+class HiddenWidgetTestForm(forms.Form):
+    secret = forms.CharField(widget=forms.HiddenInput)
+
+class HiddenWidgetTestCase(BootstrapFieldTestCase):
+    """Test HTML generation of hidden widget."""
+    form_class = HiddenWidgetTestForm
+    tests = [
+        {
+            "field": "secret",
+            "html": (
+                '<input class="form-control" id="id_secret" name="secret" type="hidden">'
+            ),
+        },
+        {
+            "field": "secret",
+            "data": {},
+            "html": (
+                '<input class="form-control" id="id_secret" name="secret" type="hidden">'
+            ),
+        },
+        {
+            "field": "secret",
+            "value": "alphabet",
+            "html": (
+                '<input class="form-control" id="id_secret" name="secret" type="hidden"'
+                ' value="alphabet">'
+            ),
+        },
+    ]
