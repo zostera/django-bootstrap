85a8a3342de8cb82a4d377745249eafd3622031b
Dylan Verheul <dylan@zostera.nl>
2020-06-06 14:57:18 +0200

    WIP

--- tests/test_widget_multiple_hidden.py
+++ tests/test_widget_multiple_hidden.py
@@ -3,56 +3,28 @@ from django import forms
 from tests.test_bootstrap_field import BootstrapFieldTestCase
 
 
-class GenderWidgetTestForm(forms.Form):
-    gender = forms.ChoiceField(label="Gender", choices=(("F", "she/her"), ("M", "he/him"), ("X", "they/their")))
+class MultipleHiddenWidgetTestForm(forms.Form):
+    gender = forms.MultipleChoiceField(label="Gender", choices=(("F", "she/her"), ("M", "he/him"), ("X", "they/their")), widget=forms.MultipleHiddenInput)
 
 
-
-class GenderWidgetTestCase(BootstrapFieldTestCase):
+class MultipleHiddenWidgetTestCase(BootstrapFieldTestCase):
     """Test HTML generation of gender widget."""
-    form_class = GenderWidgetTestForm
+    form_class = MultipleHiddenWidgetTestForm
     tests = [
         {
             "field": "gender",
-            "html": (
-                '<div class="form-group">'
-                '<label for="id_gender">Gender</label>'
-                '<input class="form-control" id="id_gender" name="gender" required type="gender">'
-                "</div>"
-            ),
+            "html": "",
         },
         {
             "field": "gender",
             "data": {},
-            "html": (
-                '<div class="form-group">'
-                '<label for="id_gender">Gender</label>'
-                '<input class="form-control" id="id_gender" name="gender" required type="gender">'
-                '<small class="form-text text-danger">This field is required.</small>'
-                "</div>"
-            ),
+            "html": "",
         },
         {
             "field": "gender",
-            "value": "alphabet",
+            "value": "F",
             "html": (
-                '<div class="form-group">'
-                '<label for="id_gender">Gender</label>'
-                '<input class="form-control" id="id_gender" name="gender" required type="gender"'
-                ' value="alphabet">'
-                '<small class="form-text text-danger">Enter a valid gender gender.</small>'
-                "</div>"
-            ),
-        },
-        {
-            "field": "gender",
-            "value": "name@example.com",
-            "html": (
-                '<div class="form-group">'
-                '<label for="id_gender">Gender</label>'
-                '<input class="form-control" id="id_gender" name="gender" required type="gender"'
-                ' value="name@example.com">'
-                "</div>"
+                '<input class="form-control" id="id_gender" name="gender" type="hidden" value="F">'
             ),
         },
     ]
